b4c5ab58f89bd0d13a7adf1430917e29e6f845be
sayyidfaruk <126539714+sayyidfaruk@users.noreply.github.com>
2024-05-26 22:15:02 +0700

    update

--- dashboard.py
+++ dashboard.py
@@ -1,24 +1,35 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
 
-sns.set_theme(style='dark')
-
-def create_daily_sharing_df(df):
-    daily_sharing_df = df.resample(rule="D", on='dteday').agg({
+def create_monthly_sharing_df(df):
+    monthly_sharing_df = df.resample(rule="M", on='dteday').agg({
         'cnt': 'sum',
         'registered': 'sum',
         'casual': 'sum',
     })
 
-    daily_sharing_df = daily_sharing_df.reset_index()
-    daily_sharing_df.rename(columns={
+    monthly_sharing_df.rename(columns={
         'cnt': 'total',
     }, inplace=True)
 
-    return daily_sharing_df
+    return monthly_sharing_df
+
+def create_yearly_sharing_df(df):
+    yearly_sharing_df = df.groupby(by='yr').agg({
+        'cnt': 'sum',
+        'registered': 'sum',
+        'casual': 'sum',
+    })
+
+    yearly_sharing_df.rename(columns={
+        'cnt': 'total',
+    }, inplace=True)
+
+    yearly_sharing_df = yearly_sharing_df.rename(axis=0, mapper={0: '2011', 1: '2012'})
+
+    return yearly_sharing_df
 
 def create_season_sharing_df(df):
     season_sharing_df = df.groupby(by='season').agg({
@@ -53,7 +64,7 @@ with st.sidebar:
     st.image('icon.png')
 
     start_date, end_date = st.date_input(
-        label='Rentang Waktu', min_value=min_date,
+        label='Time Span', min_value=min_date,
         max_value=max_date,
         value=[min_date, max_date]
     )
@@ -61,8 +72,9 @@ with st.sidebar:
 main_df = data_df[(data_df['dteday']>=str(start_date))&
                   (data_df['dteday']<=str(end_date))]
 
-daily_sharing_df = create_daily_sharing_df(main_df)
 season_sharing_df = create_season_sharing_df(main_df)
+monthly_sharing_df = create_monthly_sharing_df(main_df)
+yearly_sharing_df = create_yearly_sharing_df(main_df)
 
 st.header('Bike Sharing Dashboard :sparkles:')
 st.subheader('Bike sharing')
@@ -70,61 +82,102 @@ st.subheader('Bike sharing')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    total = daily_sharing_df.total.sum()
+    total = monthly_sharing_df.total.sum()
     st.metric('Total', value=f"{total:,}")
 
 with col2:
-    registered = daily_sharing_df.registered.sum()
+    registered = monthly_sharing_df.registered.sum()
     st.metric('Registered', value=f"{registered:,}")
     
 with col3:
-    casual = daily_sharing_df.casual.sum()
+    casual = monthly_sharing_df.casual.sum()
     st.metric('Casual', value=f"{casual:,}")
 
 fig, ax = plt.subplots(figsize=(16, 8))
-
 ax.plot(
-    daily_sharing_df['dteday'],
-    daily_sharing_df['total'],
+    monthly_sharing_df.index,
+    monthly_sharing_df['total'],
+    color="#D90404",
     linewidth=2,
-    color='#D90404',
     label='Total'
 )
-
-ax.set_title('Number of Bike sharing', loc='center',fontsize=30)
+ax.plot(
+    monthly_sharing_df.index,
+    monthly_sharing_df['registered'],
+    color="#09A603",
+    linewidth=2,
+    label='Registered'
+)
+ax.plot(
+    monthly_sharing_df.index,
+    monthly_sharing_df['casual'],
+    color="#F29F05",
+    linewidth=2,
+    label='Casual'
+)
+ax.set_title('Number of orders per month', loc='center',fontsize=30)
 ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='x', labelsize=20)
 ax.legend()
 
 st.pyplot(fig)
 
+last_5_months_df = monthly_sharing_df.tail(5)
+last_5_months_df.index = last_5_months_df.index.strftime('%B')
+
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(
-    daily_sharing_df['dteday'],
-    daily_sharing_df['total'],
+    last_5_months_df.index,
+    last_5_months_df['total'],
+    color="#D90404",
     linewidth=2,
-    color='#D90404',
+    marker='o',
     label='Total'
 )
-
 ax.plot(
-    daily_sharing_df['dteday'],
-    daily_sharing_df['registered'],
+    last_5_months_df.index,
+    last_5_months_df['registered'],
     color="#09A603",
+    linewidth=2,
+    marker='o',
     label='Registered'
 )
-
 ax.plot(
-    daily_sharing_df['dteday'],
-    daily_sharing_df['casual'],
+    last_5_months_df.index,
+    last_5_months_df['casual'],
     color="#F29F05",
+    linewidth=2,
+    marker='o',
     label='Casual'
 )
-
-ax.set_title('Number of Bike sharing with registered and casual', loc='center',fontsize=30)
+ax.set_title('Number of orders last 5 month', loc='center',fontsize=30)
 ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='x', labelsize=20)
 ax.legend()
+
+st.pyplot(fig)
+
+labels = yearly_sharing_df.index
+registered = yearly_sharing_df['registered']
+casual = yearly_sharing_df['casual']
+total = yearly_sharing_df['total']
+x = np.arange(len(labels))
+width = 0.25
+
+fig, ax = plt.subplots(figsize=(16, 8))
+rects1 = ax.bar(x - width, registered, width, label='Registered', color='#F29F05')
+rects2 = ax.bar(x, casual, width, label='Casual', color='#09A603')
+rects3 = ax.bar(x + width, total, width, label='Total', color='#D90404')
+
+ax.set_ylabel(None)
+ax.set_title('Yearly Sharing Data', fontsize=30)
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=25)
+ax.legend()
+
+fig.tight_layout()
 
 st.pyplot(fig)
 
@@ -133,7 +186,6 @@ registered = season_sharing_df[('registered', 'sum')]
 casual = season_sharing_df[('casual', 'sum')]
 total = season_sharing_df[('total', 'sum')]
 x = np.arange(len(labels))
-width = 0.25
 
 fig, ax = plt.subplots(figsize=(16, 8))
 rects1 = ax.bar(x - width, registered, width, label='Registered', color='#F29F05')
